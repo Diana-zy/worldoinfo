d16e20474e62c1c8370a0fffddad4b71021118ca
huangwei <1399852582@qq.com>
2025-06-10 17:43:03 +0800

    双词条

--- pages/detail/_detail.vue
+++ pages/detail/_detail.vue
@@ -15,7 +15,13 @@
         preload
       />
       <!-- eslint-disable vue/no-v-html -->
-      <div class="news-detail" v-html="newInfo.content"></div>
+      <!--      <div class="news-detail" v-html="newInfo.content"></div>-->
+      <div class="news-detail">
+        <div v-for="(item, index) in contentItems" :key="index">
+          <div v-if="item.type === 'content'" v-html="item.content"></div>
+          <div v-else id="relatedsearches2"></div>
+        </div>
+      </div>
       <!--eslint-enable-->
     </article>
     <Footer :lang="newInfo.language" />
@@ -43,7 +49,8 @@ export default {
   },
   data() {
     return {
-      channelId: ""
+      channelId: "",
+      maxScrollPercentage: 0
     };
   },
   head() {
@@ -96,6 +103,43 @@ export default {
       ]
     };
   },
+  computed: {
+    contentItems() {
+      const self = this;
+      const parts = this.newInfo.content.split(/(<p[^>]*>.*?<\/p>)/gs);
+      let charCount = 0;
+      const items = [];
+      const buffer = parts
+        .filter((part) => !(!part.trim() || !part.startsWith("<p")))
+        .map((part) => part.replace(/<[^>]+>/g, ""));
+      // eslint-disable-next-line no-unused-vars
+      let splitNum = Math.floor(buffer.join("").length / 3);
+      parts.forEach((part) => {
+        if (!part.trim()) return; // 跳过空字符串
+
+        // 添加内容
+        items.push({
+          type: "content",
+          content: part
+        });
+
+        // 如果不是p标签，不计算字符数和插入广告
+        if (!part.startsWith("<p")) return;
+
+        // 计算纯文本长度
+        const textContent = part.replace(/<[^>]+>/g, "");
+        charCount += textContent.length;
+        if (charCount >= splitNum && self.$route.query.doubleEntries === "true") {
+          items.push({
+            type: "ad"
+          });
+          splitNum = buffer.join("").length + 1;
+        }
+      });
+
+      return items;
+    }
+  },
   mounted: function () {
     // 获取 URL 查询参数
     const searchParams = new URLSearchParams(window.location.search);
@@ -116,9 +160,19 @@ export default {
     setTimeout(() => {
       this.addAdSenseScript();
     }, 0);
+
+    this.handleListenerScroll();
+
+    window.addEventListener("beforeunload", () => {
+      window.dataLayer.push({
+        event: "scroll_depth" + "_" + this.handleFormat(this.maxScrollPercentage),
+        hi_depth: this.handleFormat(this.maxScrollPercentage)
+      });
+    });
   },
   methods: {
     addAdSenseScript() {
+      const self = this;
       console.log("addAdSenseScript", this.newInfo.terms);
       // 获取 URL 查询参数
       const searchParams = new URLSearchParams(window.location.search);
@@ -138,7 +192,9 @@ export default {
         adsafe: "low",
         ignoredPageParams,
         relatedSearchTargeting: "content",
-        resultsPageBaseUrl: `${window.location.origin}/search/?afs&channel=${this.channelId}`,
+        resultsPageBaseUrl: `${window.location.origin}/search/?afs&channel=${this.channelId}${
+          self.$route.query?.doubleEntries === "true" ? "&doubleEntries=true" : ""
+        }`,
         resultsPageQueryParam: "query",
         terms: terms || this.newInfo.terms,
         referrerAdCreative: terms || this.newInfo.referrer_ad_creative,
@@ -155,47 +211,114 @@ export default {
           relatedSearchTargeting: "query",
           query: terms ? terms.split(",")[0] : this.newInfo.terms.split(",")[0],
           ivt: false,
-          resultsPageBaseUrl: `${window.location.origin}/search/?afs&channel=${this.channelId}`,
+          resultsPageBaseUrl: `${window.location.origin}/search/?afs&channel=${this.channelId}${
+            self.$route.query?.doubleEntries === "true" ? "&doubleEntries=true" : ""
+          }`,
           resultsPageQueryParam: "query"
         };
       }
 
       // 初始化 _googCsa 并加载相关搜索广告
       // eslint-disable-next-line no-undef
-      _googCsa("relatedsearch", adSenseConfig, {
-        container: "relatedsearches1", // 广告容器 ID
-        relatedSearches: 10, // 相关搜索广告数量
-        adLoadedCallback: function (loaded, response, isExperimentVariant, callbackOptions) {
-          console.log("adLoadedCallback", loaded, response, isExperimentVariant, callbackOptions);
-          if (response) {
-            // eslint-disable-next-line no-undef
-            dataLayer.push({ event: "C_AC" }); // 事件推送到 dataLayer
-            try {
-              let numberOfKeys = 0;
-              let concatenatedKeys = "miss";
-              if (callbackOptions.termPositions) {
-                const keys = Object.keys(callbackOptions.termPositions);
-                numberOfKeys = keys.length;
-                concatenatedKeys = keys.join(",");
-              }
-              const element = document.getElementById("master-1");
-              const height = parseFloat(element.style.height);
-              const result = Math.round(height / 105);
-
+      _googCsa(
+        "relatedsearch",
+        adSenseConfig,
+        {
+          container: "relatedsearches1", // 广告容器 ID
+          relatedSearches: self.$route.query.doubleEntries === "true" ? 5 : 10, // 相关搜索广告数量
+          adLoadedCallback: function (loaded, response, isExperimentVariant, callbackOptions) {
+            console.log("adLoadedCallback", loaded, response, isExperimentVariant, callbackOptions);
+            if (response) {
               // eslint-disable-next-line no-undef
-              dataLayer.push({
-                event: "C_AC_IN",
-                num: result,
-                queryNum: 10,
-                key1: numberOfKeys,
-                key2: concatenatedKeys
-              }); // 事件推送到 dataLayer
-            } catch (e) {
-              console.log(e);
+              dataLayer.push({ event: "C_AC" }); // 事件推送到 dataLayer
+              try {
+                let numberOfKeys = 0;
+                let concatenatedKeys = "miss";
+                if (callbackOptions.termPositions) {
+                  const keys = Object.keys(callbackOptions.termPositions);
+                  numberOfKeys = keys.length;
+                  concatenatedKeys = keys.join(",");
+                }
+                const element = document.getElementById("master-1");
+                const height = parseFloat(element.style.height);
+                const result = Math.round(height / 105);
+
+                // eslint-disable-next-line no-undef
+                dataLayer.push({
+                  event: "C_AC_IN",
+                  num: result,
+                  queryNum: self.$route.query.doubleEntries === "true" ? 5 : 10,
+                  key1: numberOfKeys,
+                  key2: concatenatedKeys
+                }); // 事件推送到 dataLayer
+              } catch (e) {
+                console.log(e);
+              }
+            }
+          }
+        },
+        self.$route.query?.doubleEntries === "true" && {
+          container: "relatedsearches2", // 广告容器 ID
+          relatedSearches: 5, // 相关搜索广告数量
+          adLoadedCallback: function (loaded, response, isExperimentVariant, callbackOptions) {
+            if (response) {
+              // eslint-disable-next-line no-undef
+              dataLayer.push({ event: "C_AC_SECOND" }); // 事件推送到 dataLayer
+              try {
+                let numberOfKeys = 0;
+                let concatenatedKeys = "miss";
+                if (callbackOptions.termPositions) {
+                  const keys = Object.keys(callbackOptions.termPositions);
+                  numberOfKeys = keys.length;
+                  concatenatedKeys = keys.join(",");
+                }
+                const element = document.getElementById("relatedsearches2");
+                const height = parseFloat(element.clientHeight);
+                const result = Math.round(height / 105);
+
+                // eslint-disable-next-line no-undef
+                dataLayer.push({
+                  event: "C_AC_IN_SECOND",
+                  queryNum: 5,
+                  num: result,
+                  key1: numberOfKeys,
+                  key2: concatenatedKeys
+                }); // 事件推送到 dataLayer
+              } catch (e) {
+                console.log(e);
+              }
             }
           }
         }
+      );
+    },
+    handleListenerScroll() {
+      const self = this;
+      window.addEventListener("scroll", (e) => {
+        // 获取当前滚动位置
+        const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+        // 获取文档总高度（减去视口高度得到可滚动高度）
+        const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+        const clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
+        // 计算滚动百分比（0-100）
+        const currentScrollPercentage =
+          scrollHeight > clientHeight
+            ? Math.min(100, (scrollTop / (scrollHeight - clientHeight)) * 100).toFixed(0) // 确保不超过100%
+            : 0;
+        if (Number(currentScrollPercentage) > Number(self.maxScrollPercentage)) {
+          self.maxScrollPercentage = currentScrollPercentage;
+        }
       });
+    },
+    handleFormat(val) {
+      if (val <= 20) {
+        return "0_20%";
+      } else if (val >= 80) {
+        return "81_100%";
+      } else {
+        return `${Math.floor(val / 20) * 2}1_${Math.floor(val / 20) * 2 + 2}0%`;
+      }
     }
   }
 };
